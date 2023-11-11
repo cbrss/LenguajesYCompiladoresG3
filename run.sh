1987ae0b9d12512b43a3bfd976199bc983f728ca
cbrss <cberrioslima@alumno.unlam.edu.ar>
2023-11-11 15:30:13 -0300

    run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ flex lexico.l
 bison -dyv sintactico.y
 gcc lex.yy.c y.tab.c tab_simb.c arbol.c pila.c cola.c -o lyc-compiler-2.0.0.jar
 ./lyc-compiler-2.0.0.jar "casos_de_prueba/test.txt"
+dot -Tpng intermediate-code.dot -o arbol.png
 rm lex.yy.c
 rm y.tab.c
 rm y.output
