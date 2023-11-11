282d1ee98b2606555f967a540601ac859bc46d73
cbrss <cberrioslima@alumno.unlam.edu.ar>
2023-11-11 20:56:40 -0300

    fix run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ flex lexico.l
 bison -dyv sintactico.y
 
 gcc.exe lex.yy.c y.tab.c tab_simb.c arbol.c pila.c cola.c -o lyc-compiler-2.0.0.jar
-lyc-compiler-2.0.0.jar casos_de_prueba/testSimple.txt
+lyc-compiler-2.0.0.jar casos_de_prueba/test.txt
 dot -Tpng intermediate-code.dot -o arbol.png
 
 @echo off
