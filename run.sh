4bf7fcbace084a29de1a5031e323ba513e0f7cb3
cbrss <cberrioslima@alumno.unlam.edu.ar>
2023-11-20 10:32:39 -0300

    run.bat arreglado

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 ## Script para Unix
 flex lexico.l
 bison -dyv sintactico.y
-gcc lex.yy.c y.tab.c tab_simb.c arbol.c pila.c cola.c -o lyc-compiler-2.0.0.jar
-./lyc-compiler-2.0.0.jar "casos_de_prueba/test.txt"
+gcc lex.yy.c y.tab.c ./src/tab_simb.c ./src/arbol.c ./src/pila.c ./src/cola.c ./src/assembler.c -o lyc-compiler-3.0.0
+lyc-compiler-3.0.0 "casos_de_prueba/test.txt"
 dot -Tpng intermediate-code.dot -o arbol.png
 rm lex.yy.c
 rm y.tab.c
 rm y.output
 rm y.tab.h
-rm lyc-compiler-2.0.0.jar
+rm lyc-compiler-3.0.0
--- runAssm.bat
+++ runAssm.bat
@@ -1,12 +1,12 @@
 PATH=D:\compilador\LenguajesYCompiladoresG3;
 
-tasm number.asm
-tasm final.asm
-tlink final.obj number.obj
+asm\TASM\tasm asm\numbers.asm
+asm\TASM\tasm final.asm
+asm\TASM\tlink /3 /x /v /k final.obj numbers.obj
 final.exe
 del final.obj 
-del number.obj 
+del numbers.obj 
 del final.exe
-del final.map
+
 
 
